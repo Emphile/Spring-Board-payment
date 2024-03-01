de73251bed1e27d44c23fb6bcbeb499a0cbcd827
emmanuelassam_Ucm3Xn <eassam89@gmail.com>
2024-03-01 17:36:09 +0000

    paymen setup

---
+++ App.py/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///school.db'
+db = SQLAlchemy(app)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    payments = db.relationship('Payment', backref='student', lazy=True)
+
+class Payment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    payment_date = db.Column(db.DateTime, default=datetime.utcnow)
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+
+@app.route('/')
+def index():
+    students = Student.query.all()
+
+
